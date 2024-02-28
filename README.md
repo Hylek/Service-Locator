ab25128bd246358f106067611d7b1104e4668734
Daniel Cumbor <daniel@lettheinsideout.com>
2024-02-28 18:54:33 +0000

    Created new custom package for my commonly used Locator class.

---
+++ README.md
@@ -0,0 +1,5 @@
+# Service Locator
+
+The Service Locator is a simple package that implements the service locator pattern in Unity, a simple and straightforward way to place all service classes under one roof for easy accessibility!
+
+This is commonly used in many of my personal projects, as such I have created a package for it so it can be updated in one location rather than per project.
---
+++ Runtime/Locator.cs
@@ -0,0 +1,75 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DC.ServiceLocator
+{
+    /// <summary>
+    /// Implements the Locator pattern so all service classes can be placed under one roof.
+    /// </summary>
+    public static class Locator
+    {
+        private static readonly Dictionary<Type, object> Services;
+        
+        static Locator() => Services = new Dictionary<Type, object>();
+
+        /// <summary>
+        /// Add a new service to the locator.
+        /// </summary>
+        /// <param name="service">The initialised service object to be added.</param>
+        /// <param name="serviceUnique">Whether there should only ever be 1 instance of the given service.</param>
+        /// <typeparam name="T">The type of service to add.</typeparam>
+        public static void AddNewService<T>(object service, bool serviceUnique = true)
+        {
+            var type = typeof(T);
+            
+            // Check if this service already exists
+            if (serviceUnique)
+            {
+                if (Services.Any(activeService => activeService.Key == type))
+                {
+                    // If the service is declared unique and it already exists; then no new service needs to be added.
+                    return;
+                }
+            }
+            
+            Services.Add(type, service);
+        }
+
+        /// <summary>
+        /// Removes a service from the locator.
+        /// </summary>
+        /// <typeparam name="T">The type of service to remove.</typeparam>
+        public static void RemoveService<T>() => Services.Remove(typeof(T));
+
+        /// <summary>
+        /// Finds and returns a service for use.
+        /// </summary>
+        /// <typeparam name="T">The type of service to request.</typeparam>
+        /// <returns></returns>
+        /// <exception cref="ApplicationException">Requested service was not found.</exception>
+        public static T Find<T>()
+        {
+            try
+            {
+                return (T)Services[typeof(T)];
+            }
+            catch
+            {
+                throw new ApplicationException("The requested service could not be found!");
+            }
+        }
+
+        /// <summary>
+        /// Checks if the service exists, useful for checking if a service already exists before adding a new one.
+        /// </summary>
+        /// <param name="type">The type of service to check for.</param>
+        /// <returns>True if the service does exist within the locator, and false if not.</returns>
+        public static bool DoesServiceExist(Type type) => Services.Any(s => s.Key == type);
+
+        /// <summary>
+        /// Cleans up the locator by removing all services.
+        /// </summary>
+        public static void CleanUp() => Services.Clear();
+    }
+}
